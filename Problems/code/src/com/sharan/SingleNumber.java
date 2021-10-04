10c87a097dd614283fcf0c6261ccb80a11aa79ac
sharansukesh1003 <sharansukesh2000@gmail.com>
2021-10-05 01:15:01 +0530

    Bitwise Problems

---
+++ Problems/code/src/com/sharan/SingleNumber.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/single-number/
+package com.sharan;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int answer = 0;
+        for(int i = 0; i < nums.length; i++){
+            answer ^= nums[i];
+        }
+        return answer;
+    }
+}
